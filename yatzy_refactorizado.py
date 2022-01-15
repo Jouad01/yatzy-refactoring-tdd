20de2676c8d5504c9b318bbc5147bb5a606dcf47
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-01-15 22:50:43 +0100

    refactor: Refactorizado con mejoras y añadido pips

--- yatzy_refactorizado.py
+++ yatzy_refactorizado.py
@@ -1,3 +1,7 @@
+from turtle import st
+from pips import Pips
+
+
 class Yatzy:
 
     def __init__(self, *dice):
@@ -6,105 +10,105 @@ class Yatzy:
     @staticmethod
     def chance(*dice):
         return sum(dice)
-    
+
     @staticmethod
     def yatzy(*dice):
         if dice.count(dice[0]) != 5:
             return 0
         return 50
-    
+
     @staticmethod
     def ones(*dice):
+        ONE = Pips.ONE.value
         return dice.count(1) * 1
 
     @staticmethod
     def twos(*dice):
+        TWO = Pips.TWO.value
         return dice.count(2) * 2
 
-# Pendiente refactorizar funcion threes a sixes
+# Pendiente refactorizar funcion threes a sixes - Finalizado
 
     @staticmethod
     def threes(*dice):
+        THREE = Pips.THREE.value
         number = dice.count(3)
         return number * 3
 
     @staticmethod
     def fours(*dice):
+        FOUR = Pips.FOUR.value
         number = dice.count(4)
         return number * 4
 
     @staticmethod
     def fives(*dice):
+        FIVE = Pips.FIVE.value
         number = dice.count(5)
         return number * 5
 
     @staticmethod
     def sixes(*dice):
+        SIX = Pips.SIX.value
         number = dice.count(6)
         return number * 6
-    
+
     @staticmethod
     def score_pair(*dice):
         PAIR = 2
-        for number in range(6, 0, -1):
-            if dice.count(number) >= PAIR:
-                return PAIR * number
+        for pip in Pips.reversedRange():
+            if dice.count(pip) >= PAIR:
+                return PAIR * pip
         return 0
 
-# Mejorable
-    @staticmethod
-    def two_pairs(*dice):
+# Fino
+    
+    @classmethod
+    def two_pairs(cls, *dice):
         PAIR = 2
-        pairs = 0
-        total = 0
-        number = 1
-        while pairs < 2 and number <= 6:
-            if dice.count(number) >= 2:
-                pairs += 1
-                total += PAIR * number
-            number += 1
-        if pairs == 2:
-            return total
-        return 0
+        pips_pairs = cls.__filter_pips_repeated(dice, PAIR)
+        return sum(pips_pairs) * PAIR if len(pips_pairs) == 2 else 0
 
-    @staticmethod
-    def three_of_kind(*dice):
-        for number in range(6, 0, -1):
-            if dice.count(number) >= 3:
-                return 3 * number
-        return 0
-    
-    @staticmethod
-    def four_of_kind(*dice):
-        for number in range(6, 0, -1):
-            if dice.count(number) >= 4:
-                return 4 * number
-        return 0
-    
-    @staticmethod
-    def small_straight(*dice):
-        for number in range(1, 6):
-            if dice.count(number) != 1:
-                return 0
-        return Yatzy.chance(*dice)
-    
-    @staticmethod
-    def large_straight(*dice):
-        for number in range(2, 7):
-            if dice.count(number) != 1:
-                return 0
-        return Yatzy.chance(*dice)
+    @classmethod
+    def three_of_kind(cls, *dice):
+        THREE = 3
+        pip = cls.__biggest_pip_repeated(dice, THREE)
+        return pip * THREE if pip else 0
 
-    @staticmethod
-    def full_house(*dice):
-        if Yatzy.down_pair(*dice) and Yatzy.three_of_kind(*dice):
-            return Yatzy.down_pair(*dice) + Yatzy.three_of_kind(*dice)
+    @classmethod
+    def four_of_kind(cls, *dice):
+        FOUR = 4
+        pip = cls.__biggest_pip_repeated(dice, FOUR)
+        return pip * FOUR if pip else 0
+
+    @classmethod
+    def __biggest_pip_repeated(cls, dice, times):
+        pips = cls.__filter_pips_repeated(dice, times)
+        return pips[0] if pips else []
+
+    @classmethod
+    def __filter_pips_repeated(cls, dice, times):
+        return list(filter(lambda pip: dice.count(pip) >= times, Pips.reversedRange()))
+
+    @classmethod
+    def small_straight(cls, *dice):
+        return cls.chance(*dice) if not Pips.minus(Pips.SIX) - set(dice) else 0
+
+    @classmethod
+    def large_straight(cls, *dice):
+        return cls.chance(*dice) if not Pips.minus(Pips.ONE) - set(dice) else 0
+
+    @classmethod
+    def full_house(cls, *dice):
+        if cls.down_pair(*dice) and cls.three_of_kind(*dice):
+            return cls.down_pair(*dice) + cls.three_of_kind(*dice)
+        else:
+            return 0
+
+    @classmethod
+    def down_pair(cls, *dice):
+        PAIR = 2
+        for pip in Pips.reversedRange():
+            if dice.count(pip) == PAIR:
+                return PAIR * pip
         return 0
-    
-    @staticmethod
-    def down_pair(*dice):
-        for number in range(6, 0, -1):
-            if dice.count(number) == 2:
-                return 2 * number
-        return 0
-        
